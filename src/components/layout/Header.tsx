15d829318b1b43f8bc9bf22eeba2fe509fa33e4b
abhinavsiwal <abhinavsiwal@gmail.com>
2025-10-08 01:54:16 +0530

    Header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ChevronDown } from 'lucide-react';
+
+const Header = () => {
+  return (
+    <div className='bg-primary h-16 flex items-center justify-between px-4'>
+        <p className="text-white text-4xl font-bold">JMP</p>
+        <div className="rounded-full bg-primary p-2 flex items-center gap-2 shadow-2xl">
+            <p className="rounded-full bg-gray-800 p-2 text-white">DO</p>
+            <ChevronDown className="text-white" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
