6a8268ecff8cb18afdb2589353c3e57705cb786c
abhinavsiwal <abhinavsiwal@gmail.com>
2025-10-08 14:38:31 +0530

    Added Date Picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import Image from "next/image";
+import { DateRangePicker } from "@/components/ui/data-range-picker";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
 
 export default function Home() {
   return (
@@ -9,8 +14,36 @@ export default function Home() {
       <p className="text-sm text-gray-700 underline font-medium ">Last updated: 10/08/2025 10:00 AM</p>
       </div>
 
-      <div className="w-full h-full rounded-xl p-6 border border-black py-12">
+      <div className="w-full h-full rounded-xl p-6 border border-black py-12 flex flex-col gap-6">
         <h6 className="text-black text-2xl font-bold" >Overall Business Performance</h6>
+        <div className="w-full flex items-center gap-8">
+          <div className="flex flex-col gap-2">
+            <p className="text-gray-700 text-sm font-medium" >Date Range</p>
+            <DateRangePicker
+            onUpdate={(values) => console.log(values)}
+            initialDateFrom="2025-01-01"
+            initialDateTo="2025-12-31" 
+            align="start"
+            locale="en-GB"
+            showCompare={false}
+          
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-gray-700 text-sm font-medium" >Compare</p>
+         <Select >
+          <SelectTrigger>
+            <SelectValue placeholder="Year Over Year" />
+            <SelectContent>
+              <SelectItem value="1">Year Over Year</SelectItem>
+              <SelectItem value="2">Quarter Over Quarter</SelectItem>
+              <SelectItem value="3">Month Over Month</SelectItem>
+            </SelectContent>
+          </SelectTrigger>
+         </Select>
+          </div>
+    
+        </div>
       </div>
    
     </div>
